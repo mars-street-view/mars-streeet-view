dcd420373c08a007f660de6fa3a82b6dd2536a92
paulsheridan <paul.sheridan@me.com>
2016-03-28 13:09:53 -0700

    Rover and Photos models created, foreign keys added.

--- mars_street_view/models.py
+++ mars_street_view/models.py
@@ -24,4 +24,27 @@ class MyModel(Base):
     name = Column(Text)
     value = Column(Integer)
 
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
+
+class Rover(Base):
+    __tablename__ = 'rovers'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    landing_date = Column(String, nullable=False)
+    max_date = Column(String, nullable=False)
+    max_sol = Column(String, nullable=False)
+    total_photos = Column(Integer, nullable=False)
+    photos = relationship('Photo', back_populates='rover')
+
+
+
+class Photo(Base):
+    __tablename__ = 'photos'
+    id = Column(Integer, primary_key=True)
+    img_src = Column(String, nullable=False)
+    sol = Column(Integer, nullable=False)
+    earth_date = Column(String, nullable=False)
+    rover = relationship('Rover', back_populates='photos')
+    rover_id = Column(Integer, ForeignKey('rovers.id'))
+
+# Index('my_index', MyModel.name, unique=True, mysql_length=255)
+# TODO: write indexes for forthcoming tables.
