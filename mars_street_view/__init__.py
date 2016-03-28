86c318c33021c1629797edcbd854ee8865362cfe
Will Weatherford <weatherford.william@gmail.com>
2016-03-28 10:41:05 -0700

    Stable launch of pserve. DB needs to be initialize

--- mars_street_view/__init__.py
+++ mars_street_view/__init__.py
@@ -8,13 +8,12 @@ from .models import (
 
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
+    """Return a Pyramid WSGI application."""
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
+    config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
