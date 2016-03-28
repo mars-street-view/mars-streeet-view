a478d7837a5b3a5556b48c44c2024abe33c00e25
muniri92 <mibrah04@gmail.com>
2016-03-28 11:51:06 -0700

    got a few tests running, and parsed json file is on point

--- api_call.py
+++ api_call.py
@@ -1,8 +1,10 @@
 # _*_ Coding: utf-8 _*_
 
 import os
+import io
 import requests
-from sys import argv
+import json
+# from sys import argv
 
 CURIOSITY = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos'
 OPPORTUNITY = 'https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos'
@@ -14,7 +16,9 @@ INSPECTION_PARAMS = {
     'api_key': NASA_API,
 }
 
+
 def get_inspection_page(rover):
+    """Make API call to NASA."""
     url = ""
     if rover == 'spirit':
         url = SPIRIT
@@ -29,11 +33,28 @@ def get_inspection_page(rover):
     #         params[key] = val
     params['sol'] = 522
     resp = requests.get(url, params=params)
-    import pdb; pdb.set_trace()
-    resp.raise_for_status() # <- This is a no-op if there is no HTTP error
+    # import pdb; pdb.set_trace()
+    resp.raise_for_status()  # <- This is a no-op if there is no HTTP error
     # remember, in requests `content` is bytes and `text` is unicode
     return resp.content, resp.encoding
 
-if __name__ == '__main__':
-    get_inspection_page(rover='curiostiy')
 
+def write_to_file(resp, file_name):
+    """Save JSON to a file."""
+    content, encoding = resp
+    file = io.open(file_name, encoding='utf-8', mode='w')
+    file.write(content.decode(encoding))
+    file.close()
+
+
+def read_json(file):
+    """Parse JSON."""
+    text = io.open(file, encoding='utf-8', mode='r')
+    unparsed = text.read()
+    parsed = json.loads(unparsed)
+    return parsed
+
+
+if __name__ == '__main__':
+    # write_to_file(get_inspection_page(rover='curiostiy'), 'sample_data.json')
+    read_json('sample_data.json')
