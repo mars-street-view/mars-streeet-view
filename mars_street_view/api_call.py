7bb95e6a24d6d3f74b3c8113abe8111953b39215
Will Weatherford <weatherford.william@gmail.com>
2016-03-29 18:23:19 -0700

    added manual function to test photo urls

--- mars_street_view/api_call.py
+++ mars_street_view/api_call.py
@@ -6,7 +6,7 @@ import os
 import io
 import requests
 import json
-# from sys import argv
+import webbrowser
 
 
 PARENT_DIR = os.path.dirname(__file__)
@@ -21,8 +21,12 @@ ROVERS = {
 NASA_API_KEY = os.environ.get('NASA_API_KEY')
 
 
-def fetch_photo_data(url, rover, sol):
+def fetch_photo_data(rover, sol, camera=None):
     """Make API call to NASA."""
+    try:
+        url = ROVERS[rover]
+    except KeyError:
+        raise ValueError('Incorrect rover name provided.')
     page = 1
     lst = []
     found_ids = set()
@@ -32,8 +36,13 @@ def fetch_photo_data(url, rover, sol):
             'page': page,
             'api_key': NASA_API_KEY,
         }
+        if camera:
+            params['camera'] = camera
         resp = requests.get(url, params=params)
-        resp.raise_for_status()  # <- This is a no-op if there is no HTTP error
+        if resp.status_code == 400:
+            print('400 response for {0} {camera} sol {sol} page={page}'
+                  ''.format(rover, **params))
+            break
         content, encoding = resp.content, resp.encoding
         photo_data = json.loads(content.decode(encoding))
         photos = photo_data['photos']
@@ -84,18 +93,29 @@ def read_json_from_file(file_name, encoding='utf-8'):
         return json.load(file)
 
 
-def get_one_sol(rover, sol, fetch=False):
+def get_one_sol(rover, sol, fetch=False, camera=None):
     """Return all photos for one sol, given rover and sol."""
-    try:
-        url = ROVERS[rover]
-    except KeyError:
-        raise ValueError('Incorrect rover name provided.')
     if fetch:
-        photo_list = fetch_photo_data(url, rover, sol)
+        photo_list = fetch_photo_data(rover, sol, camera)
     else:
         photo_list = load_photo_data(rover, sol)
     return photo_list
 
 
+def webbrowse_photos(rover_name, sol_range, camera):
+    for n in sol_range:
+        results = fetch_photo_data(rover_name, n, camera)
+        id_url_list = [(result['id'], result['img_src']) for result in results]
+        id_url_list.sort()
+        for photo_id, url in id_url_list:
+            print('opening url for id {}'.format(photo_id))
+            webbrowser.open_new_tab(url)
+
 if __name__ == '__main__':
-    fetch_and_save_data_sample()
+    import sys
+    if len(sys.argv) != 5:
+        print('Wrong num of args.')
+        sys.exit()
+    rover_name, start_sol, sol_range, camera = sys.argv[1:]
+    sol_range = range(int(start_sol), int(start_sol) + int(sol_range))
+    webbrowse_photos(rover_name, sol_range, camera)
