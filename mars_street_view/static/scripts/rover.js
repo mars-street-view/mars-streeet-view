ee3093bcbfe4603b4b105e8d9093933c8212e1e1
muniri92 <mibrah04@gmail.com>
2016-03-31 02:46:35 -0700

    we can move through images and sols for each rover

--- mars_street_view/static/scripts/rover.js
+++ mars_street_view/static/scripts/rover.js
@@ -22,50 +22,45 @@ var sol = 1;
 var count = 0;
 
 
-// // HAS NOT BEEN CREATED
-// $yesterday = $('#yesterday');
-// $tomorrow = $('#tomorrow');
+// // Constructor for multiple cameras
+// function Camera(details) {
+//     Object.keys(details).forEach(function(e, index, keys) {
+//         this[e] = details[e];
+//     }, this);
+// }
+
+// var template;
+// // Compile the template
+// Camera.prototype.compileTemplate = function(){
+//     var source = $('#cam-template').html();
+//     template = Handlebars.compile(source)
+//     return template(this)
+// }
+
+// // var a;
+// // Append the template to the buttons
+// function buildButtons(){
+//     Camera.all.forEach(function(a){
+//         if (Object.keys(a).length > 0) {
+//             console.log(a)
+//             // $('.cam-buttons').append(a[0].compileTemplate());
+//         }
+//     });
+// }
 
 
-// Constructor for multiple cameras
-function Camera(details) {
-    Object.keys(details).forEach(function(e, index, keys) {
-        this[e] = details[e];
-    }, this);
-}
-
-var template;
-// Compile the template
-Camera.prototype.compileTemplate = function(){
-    var source = $('#cam-template').html();
-    template = Handlebars.compile(source)
-    return template(this)
-}
-
-// var a;
-// Append the template to the buttons
-function buildButtons(){
-    Camera.all.forEach(function(a){
-        if (Object.keys(a).length > 0) {
-            console.log(a)
-            $('.cam-buttons').append(a[0].compileTemplate());
-        }
-    });
-}
+// // Fill list with objects
+// Camera.all = []
 
 
-// Fill list with objects
-Camera.all = []
-
-
-// Create the objects from the ajax call
-Camera.loadall = function(camList) {
-    new_list = camList.photos_by_cam
-    for (var property in new_list) {
-        console.log(property)
-        Camera.all.push(new Camera(new_list[property]))
-    }
-};
+// // Create the objects from the ajax call
+// Camera.loadall = function(camList) {
+//     new_list = camList.photos_by_cam
+//     for (var property in new_list) {
+//         console.log(property)
+//         Camera.all.push(new Camera(new_list[property]))
+//     }
+// };
 
 
 // Event Listener to run the ajax call
@@ -96,21 +91,23 @@ function fetchPhotos(rover, sol) {
             // console.log(response)
             // function to make the list of cameras
             // console.log(rover);
-            fullCameraList(rover, camList);
+            fullCameraList(rover, response);
             
-            Camera.loadall(camList)
-
-            buildButtons()
+            // Camera.loadall(camList)
+            // console.log(camList)
+            // buildButtons()
             // take the first image and change the 'src' attribute of the main photo (NAVCAM)
+            // console.log(navcam)
             $('#main-photo').attr('src', navcam[count].img_src);
+            // fetchPhotos(rover, sol);
         }
-    });
+    })   
 };
 
 
 // Returns a list of all the objects of a rover depending on the camera
 function fullCameraList(rover, camList) {
-    if (rover = 'Curiosity') {
+    if (rover === 'Curiosity') {
         navcam = camList.photos_by_cam[rover + '_NAVCAM'];
         fhaz = camList.photos_by_cam[rover + '_FHAZ'];
         rhaz = camList.photos_by_cam[rover + '_RHAZ'];
@@ -126,20 +123,24 @@ function fullCameraList(rover, camList) {
         minites = camList.photos_by_cam[rover + '_MINITES'];
         entry = camList.photos_by_cam[rover + '_ENTRY'];
     };
+    // console.log(navcam)
 };
 
 
 // Event listener for the next image to populate main image space
 $("#next-photo").on('click', function(e){
     var url = document.getElementById("main-photo").src;
-    if (count < navcam.length){
-        count += 1;
+    if (count < navcam.length - 1){
         newUrl = navcam[count].img_src;
         $('#main-photo').attr('src', newUrl);
+        count += 1;
     } else {
         count = 0;
         sol += 1;
-        fetchPhotos(rover, sol);
+        console.log(rover)
+        cap_rover = rover.charAt(0).toUpperCase() + rover.slice(1);
+        // if sol < max_sol
+        fetchPhotos(cap_rover, sol);
     }
 })
 
@@ -148,27 +149,36 @@ $("#next-photo").on('click', function(e){
 $("#prev-photo").on('click', function(e){
     var url = document.getElementById("main-photo").src;
     if (count > 0){
-        count -= 1;
         newUrl = navcam[count].img_src;
         $('#main-photo').attr('src', newUrl);
+        count -= 1;
     } else {
         count = 0;
         sol -= 1;
-        fetchPhotos(rover, sol);
+        if (sol > 0){ 
+            cap_rover = rover.charAt(0).toUpperCase() + rover.slice(1);
+            fetchPhotos(cap_rover, sol);
+        }
     }
 })
 
 
+// NEXT SOL
+$("#next-sol").on('click', function(e){
+    cap_rover = rover.charAt(0).toUpperCase() + rover.slice(1);
+    sol += 1;
+    // if sol < max_sol
+    fetchPhotos(cap_rover, sol);
+})
 
-// // ????
-// $spirit = $('#spirit');
-// $opportunity = $('#opportunity');
-// $curiosity = $('#curiosity');
-// // $mainPhoto = $('#main-photo');
 
+// PREV SOL
+$("#prev-sol").on('click', function(e){
+    cap_rover = rover.charAt(0).toUpperCase() + rover.slice(1);
+    sol -= 1;
+    if (sol > 0){ 
+        fetchPhotos(cap_rover, sol);
+    }
+})
 
-// Curiosity&sol=1
-// http://marsstreetview.com/app?rover=
-// http://marsstreetview.com/app?rover=Opportunity&sol=1
-// http://marsstreetview.com/app?rover=Spirit&sol=1
 
