347c1405f8ba48d4206632fc04f21e00f543a9a0
Will Weatherford <weatherford.william@gmail.com>
2016-03-31 11:56:15 -0700

    Another test for sequentialness

--- mars_street_view/tests/test_sequential_photos.py
+++ mars_street_view/tests/test_sequential_photos.py
@@ -17,6 +17,19 @@ from mars_street_view.models import DBSession, Photo
 #         time.sleep(1)
 
 
+def test_photos_seq(pre_pop_transaction, global_environ):
+    rover_name = random.choice(['Curiosity', 'Opportunity', 'Spirit'])
+    sol = random.choice(range(1, 5))
+    data = Photo.get_rov_sol(rover_name, sol)
+    cam = random.choice(list(data['photos_by_cam'].keys()))
+    photos = data['photos_by_cam'][cam]
+    urls_from_method = [photo.img_src for photo in photos]
+    prev_url = ''
+    for url in urls_from_method:
+        assert url > prev_url
+        prev_url = url
+
+
 def test_photos_sorted(pre_pop_transaction, global_environ):
     rover_name = random.choice(['Curiosity', 'Opportunity', 'Spirit'])
     sol = random.choice(range(1, 5))
